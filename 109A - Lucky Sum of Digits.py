fb78ed21a85e6bc0f1cccfe6f75d9cfda30aceaa
jems jr <108370284+muhammedjemal@users.noreply.github.com>
2023-01-24 17:03:28 +0300

    Create 109A - Lucky Sum of Digits.py

---
+++ 109A - Lucky Sum of Digits.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n // 7 + 1):
+    for j in range((n - 7 * (n // 7 - i)) // 4 + 1):
+        if (n // 7 - i) * 7 + j * 4 == n:
+            print("4" * j + "7" * (n // 7 - i))
+            quit()
+        elif (n // 7 - i) * 7 + j * 4 > n:
+            break
+print(-1)
